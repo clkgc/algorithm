5a2468c285f9397b5f7849d3d89bee0a56adeb1d
69is54 <chienliko66@gmail.com>
2024-01-12 18:39:08 +0800

    feat: runtime 95.31%

---
+++ Leetcode/Grind75/53.maximum-subarray.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=53 lang=javascript
+ *
+ * [53] Maximum Subarray
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    let maxEndingHere = nums[0];
+    let maxSoFar = nums[0];
+
+    for (let i = 1; i < nums.length; i++) {
+        maxEndingHere = Math.max(nums[i], maxEndingHere + nums[i]);
+        maxSoFar = Math.max(maxSoFar, maxEndingHere);
+    }
+
+    return maxSoFar;
+};
+// @lc code=end
+
