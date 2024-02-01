ac84ba60f6b78a872af3a5e5aa76c4041559ffd5
69is54 <chienliko66@gmail.com>
2024-02-01 22:56:36 +0800

    feat: 43%

---
+++ Leetcode/Grind75/206.reverse-linked-list.js
@@ -0,0 +1,74 @@
+/*
+ * @lc app=leetcode id=206 lang=javascript
+ *
+ * [206] Reverse Linked List
+ *
+ * https://leetcode.com/problems/reverse-linked-list/description/
+ *
+ * algorithms
+ * Easy (75.51%)
+ * Likes:    20536
+ * Dislikes: 399
+ * Total Accepted:    3.7M
+ * Total Submissions: 4.9M
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * Given the head of a singly linked list, reverse the list, and return the
+ * reversed list.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: head = [1,2,3,4,5]
+ * Output: [5,4,3,2,1]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: head = [1,2]
+ * Output: [2,1]
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: head = []
+ * Output: []
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in the list is the range [0, 5000].
+ * -5000 <= Node.val <= 5000
+ * 
+ * 
+ * 
+ * Follow up: A linked list can be reversed either iteratively or recursively.
+ * Could you implement both?
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+function reverseList(head, prev = null) {
+    if (!head) return prev;
+    let next = head.next;
+    head.next = prev;
+    return reverseList(next, head);
+}
+// @lc code=end
+
