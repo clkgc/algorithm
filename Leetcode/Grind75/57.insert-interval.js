6c983ef324dc00ae67ca1c449d28fe0cae427a85
69is54 <chienliko66@gmail.com>
2024-02-20 13:34:45 +0800

    feat:85%

---
+++ Leetcode/Grind75/57.insert-interval.js
@@ -0,0 +1,93 @@
+/*
+ * @lc app=leetcode id=57 lang=javascript
+ *
+ * [57] Insert Interval
+ *
+ * https://leetcode.com/problems/insert-interval/description/
+ *
+ * algorithms
+ * Medium (39.83%)
+ * Likes:    9504
+ * Dislikes: 715
+ * Total Accepted:    943.6K
+ * Total Submissions: 2.4M
+ * Testcase Example:  '[[1,3],[6,9]]\n[2,5]'
+ *
+ * You are given an array of non-overlapping intervals intervals where
+ * intervals[i] = [starti, endi] represent the start and the end of the i^th
+ * interval and intervals is sorted in ascending order by starti. You are also
+ * given an interval newInterval = [start, end] that represents the start and
+ * end of another interval.
+ * 
+ * Insert newInterval into intervals such that intervals is still sorted in
+ * ascending order by starti and intervals still does not have any overlapping
+ * intervals (merge overlapping intervals if necessary).
+ * 
+ * Return intervals after the insertion.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+ * Output: [[1,5],[6,9]]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+ * Output: [[1,2],[3,10],[12,16]]
+ * Explanation: Because the new interval [4,8] overlaps with
+ * [3,5],[6,7],[8,10].
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 0 <= intervals.length <= 10^4
+ * intervals[i].length == 2
+ * 0 <= starti <= endi <= 10^5
+ * intervals is sorted by starti in ascending order.
+ * newInterval.length == 2
+ * 0 <= start <= end <= 10^5
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} intervals
+ * @param {number[]} newInterval
+ * @return {number[][]}
+ */
+function insert(intervals, newInterval) {
+    const result = [];
+    let i = 0, n = intervals.length;
+
+    // Step 1: Add non-overlapping intervals before newInterval
+    while (i < n && intervals[i][1] < newInterval[0]) {
+        result.push(intervals[i]);
+        i++;
+    }
+
+    // Step 2: Merge overlapping intervals
+    while (i < n && intervals[i][0] <= newInterval[1]) {
+        newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+        newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+        i++;
+    }
+    result.push(newInterval); // Add the merged interval
+
+    // Step 3: Add the remaining intervals
+    while (i < n) {
+        result.push(intervals[i]);
+        i++;
+    }
+
+    return result;
+}
+
+// @lc code=end
+
